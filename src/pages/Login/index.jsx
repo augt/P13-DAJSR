e940f15f1cb6e211b202c3d100ad4025fb8f84e1
Augustin Tacquet <augu.tacquet@gmail.com>
2023-12-22 14:19:20 +0100

    removed commented code

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-/* import { Link } from "react-router-dom"; */
 import { login, getUserProfile } from "../../redux/user/userSlice";
 
 function Login() {
